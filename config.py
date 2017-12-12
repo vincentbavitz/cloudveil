0be99544b7858a371f415be16060fe56a0562ff7
Vincent Bavitz <31360195+xvol@users.noreply.github.com>
2017-12-13 04:35:42 +1100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+#Location of your encrypted files, eg. /mnt/MEGA/
+#"/absolute/path/to/sync/directory"
+sync_directory = ""
+
+#Where your unencrypted files are stored
+#"/absolute/path/to/my/input/directory/"
+input_directory = ""
+
+#Where you'd like to decrypt your files
+#"/absolute/path/to/my/landing/directory/"
+landing_directory = ""
